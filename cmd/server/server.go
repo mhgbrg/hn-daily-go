a21db569a4b801ea531ed7610096507dd46d0bc6
Mats Högberg <mats@hgbrg.se>
2019-02-28 17:39:13 +0100

    Port is now read from environment variable PORT

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"log"
+	"os"
+	"strconv"
 
 	"github.com/mhgbrg/hndaily/cmd/cmdutils"
 	"github.com/mhgbrg/hndaily/pkg/web"
+	"github.com/pkg/errors"
 )
 
 func main() {
+	portStr := os.Getenv("PORT")
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		log.Fatal(errors.Wrapf(err, "failed to parse port %s", portStr))
+	}
+
 	db := cmdutils.ConnectToDB()
 	defer db.Close()
-	log.Fatal(web.StartServer(db))
+
+	log.Fatal(web.StartServer(port, db))
 }
--- pkg/web/server.go
+++ pkg/web/server.go
@@ -2,20 +2,21 @@ package web
 
 import (
 	"database/sql"
+	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/gorilla/handlers"
 )
 
-func StartServer(db *sql.DB) error {
+func StartServer(port int, db *sql.DB) error {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", Wrap(GetLatestDigest(db)))
 	mux.HandleFunc("/digest/", Wrap(GetDigest(db)))
 	mux.HandleFunc("/story/", Wrap(ReadStory(db)))
 	mux.HandleFunc("/archive/", Wrap(Archive(db)))
 	return http.ListenAndServe(
-		"localhost:8080",
+		fmt.Sprintf("localhost:%d", port),
 		handlers.LoggingHandler(os.Stdout, mux),
 	)
 }
