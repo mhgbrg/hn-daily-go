87f0d9318cc6dae7835063a03d489eb08491aae8
Mats Högberg <mats@hgbrg.se>
2019-03-01 22:57:12 +0100

    Created go script for applying migrations

---
+++ cmd/apply_migrations/apply_migrations.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	migrate "github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+
+	"github.com/pkg/errors"
+)
+
+func main() {
+	dbURL := os.Getenv("DATABASE_URL")
+	m, err := migrate.New(
+		"file://db/migrations",
+		dbURL,
+	)
+	if err != nil {
+		log.Fatal(errors.Wrap(err, "failed to apply migrations"))
+	}
+	m.Up()
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ module github.com/mhgbrg/hndaily
 // +heroku install ./cmd/...
 
 require (
+	github.com/golang-migrate/migrate/v4 v4.2.5
 	github.com/gorilla/handlers v1.4.0
 	github.com/gorilla/sessions v1.1.3
 	github.com/lib/pq v1.0.0
